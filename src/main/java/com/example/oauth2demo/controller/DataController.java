3a57633c90c38e0ebcb5743858a6983a5dddc8f8
Ruslan <ruslan.kovshar@gmail.com>
2020-02-12 13:55:40 +0200

    add info about /api/user mapping  to swagger

--- src/main/java/com/example/oauth2demo/controller/DataController.java
+++ src/main/java/com/example/oauth2demo/controller/DataController.java
@@ -1,5 +1,6 @@
 package com.example.oauth2demo.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.security.Principal;
 @RequestMapping("/api")
 public class DataController {
 
+    @ApiOperation(value = "View info about authenticated user")
     @RequestMapping("/user")
     public Principal user(Principal principal) {
         return principal;
